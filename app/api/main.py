9405d79f068502bcd390296497e8da845fb2cfc8
ADITYA KAMBOJ <sierra.phoenix1511@gmail.com>
2026-09-16 12:28:00 +0530

    added cors

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api.indices import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://<frontend-render-url>.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 @app.api_route("/health", methods=["GET", "HEAD"])
