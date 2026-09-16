91d0a0a991eac71dc73482c578838e8e9facdd07
ADITYA KAMBOJ <sierra.phoenix1511@gmail.com>
2026-09-16 12:59:33 +0530

    Make timestamps timezone aware

--- app/db/models/index_value.py
+++ app/db/models/index_value.py
@@ -5,10 +5,19 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class IndexValue(Base):
     __tablename__ = "index_value"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+
     index_id: Mapped[int] = mapped_column(ForeignKey("security.id"), nullable=False)
+
     index_value: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    timestamp: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True),
+    nullable=False
+    )
+
     total_free_float_market_cap: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
+
     divisor: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
 
--- app/db/models/market_data.py
+++ app/db/models/market_data.py
@@ -7,10 +7,20 @@ class MarketData(Base):
     __tablename__ = "market_data"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     security_id: Mapped[int] = mapped_column(ForeignKey("security.id"), nullable=False)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False
+    )
+
     last_price: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
+
     total_market_cap: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
+
     free_float_market_cap: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
+
     impact_cost: Mapped[float] = mapped_column(Numeric(20,4), nullable=False)
+
     issued_size: Mapped[int | None] = mapped_column()
